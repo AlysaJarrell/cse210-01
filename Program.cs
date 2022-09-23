861b74f85acbca5a119cef57bed778a9df15730b
AlysaJarrell <cra20007@byui.edu>
2022-09-23 13:32:57 -0600

    ititial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Assignment CSE210-01, Tic-Tac-Toe
+// Author: Alysa Jarrell
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
